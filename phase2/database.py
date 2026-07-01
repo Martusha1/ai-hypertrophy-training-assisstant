58c6c14699a30bca14b60c5506c4ab05fabad8ce
Martin Ivanov <martusha@Martins-MacBook-Air.local>
2026-07-01 21:36:19 +0300

    SQLite database schema with users, programs, sessions and sets tables done

--- phase2/database.py
+++ phase2/database.py
@@ -8,7 +8,7 @@ def init_db():
         CREATE TABLE IF NOT EXISTS users (
         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
-        age int,
+        age INTEGER NOT NULL,
         training_experience TEXT,
         training_days_per_week INTEGER NOT NULL,
         session_length INTEGER NOT NULL,
@@ -21,12 +21,39 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS program (
         program_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id int NOT NULL,
+        user_id INTEGER NOT NULL,
         program_name TEXT,
-        weeks int NOT NULL,
+        weeks INTEGER NOT NULL,
         raw_json TEXT,
         created_at DATE,
         FOREIGN KEY (user_id) REFERENCES users(user_id)
+        );
+""")
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS workout_sessions (
+        workout_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        program_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        session_date DATE,
+        day_number INTEGER NOT NULL,
+        notes TEXT,
+        FOREIGN KEY (program_id) REFERENCES program(program_id),
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+        );
+""")
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS logged_sets (
+        sets_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        workout_id INTEGER NOT NULL,
+        exercise_name TEXT,
+        set_number INTEGER,
+        reps_done INTEGER,
+        weight_kg REAL,
+        weight_lbs REAL,
+        rir INTEGER,
+        FOREIGN KEY (workout_id) REFERENCES workout_sessions(workout_id)
         );
 """)
 
